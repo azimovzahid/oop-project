95b8e1b737f015de5d2f3d12f84d3f3a575d6ada
Elmo <azimovzahid0@gmail.com>
2024-05-16 15:06:06 +0200

    Added setters and getters to weight and body fat

--- main.cpp
+++ main.cpp
@@ -18,6 +18,20 @@ public:
     Person(double _weight, double _body_fat, double _calves, double _tights, double _back, double _chest, double _biceps): weight(_weight), body_fat(_body_fat), tights(_tights), back(_back), chest(_chest), biceps(_biceps){
         updateMuscleMass();
     }
+    double getWeight() const{
+        return weight;
+    }
+    void setWeight(double _weight) {
+        weight = _weight;
+        updateMuscleMass();
+    }
+    double getBodyFat() const{
+        return body_fat;
+    }
+    void setBodyFat(double _body_fat) {
+        body_fat = _body_fat;
+        updateMuscleMass();
+    }
 };
 
 int main() {
