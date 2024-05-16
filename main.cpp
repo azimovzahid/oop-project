efe7cecdb888cf6455cec578cc988c6565286ab3
Elmo <azimovzahid0@gmail.com>
2024-05-16 14:55:29 +0200

    Added Person class and its fields

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+class Person {
+    double weight;
+    double body_fat;
+    double calves;
+    double tights;
+    double back;
+    double chest;
+    double biceps;
+    double muscle_mass;
+};
+
 int main() {
     return 0;
 }
