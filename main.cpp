be7dba7dee41eb8e1561bb961bd1f10dfd9238af
Elmo <azimovzahid0@gmail.com>
2024-05-18 01:23:58 +0200

    Fixed Deadlift and Lunges do_exercise method

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 
+class Person;
+
 class Exercise {
 public:
-    virtual void do_exercise(double &body_part, double &body_fat) {}
+    virtual void do_exercise(Person &person, double &body_fat) {}
 };
 
 class Person {
@@ -91,9 +93,10 @@ class Lunges: public Exercise {
     int reps;
 public:
     Lunges(int _sets, int _reps): sets(_sets), reps(_reps) {}
-    void do_exercise(double &thighs, double &body_fat) override {
+    void do_exercise(Person &person, double &body_fat) override {
         double multiplier = (double)sets * reps / 100;
-        thighs = thighs * multiplier;
+        double thighs = person.getThighs() * (1 + multiplier);
+        person.setThighs(thighs);
     }
 };
 
@@ -103,9 +106,10 @@ class Deadlift: public Exercise {
     double weights;
 public:
     Deadlift(int _sets, int _reps, double _weights): sets(_sets), reps(_reps), weights(_weights) {}
-    void do_exercise(double &back, double &body_fat) override {
-        double multiplier = (double)sets * reps * weights / 100;
-        back = back * multiplier;
+    void do_exercise(Person &person, double &body_fat) override {
+        double multiplier = (double)sets * reps * (weights/10) / 100;
+        double back = person.getBodyFat() * (1+multiplier);
+        person.setBack(back);
     }
 };
 
@@ -114,7 +118,7 @@ class Pushups: public Exercise {
     int reps;
 public:
     Pushups(int _sets, int _reps): sets(_sets), reps(_reps) {}
-    void do_exercise(double &chest, double &body_fat) override {
+    void do_exercise(Person &person, double &body_fat) override {
         double multiplier = (double)sets * reps / 100;
         chest = chest * multiplier;
     }
@@ -126,7 +130,7 @@ class Dumbbell_curl: public Exercise {
     double weights;
 public:
     Dumbbell_curl(int _sets, int _reps, double _weights): sets(_sets), reps(_reps), weights(_weights) {}
-    void do_exercise(double &biceps, double &body_fat) override {
+    void do_exercise(Person &person, double &body_fat) override {
         double multiplier = (double)sets * reps * weights / 100;
         biceps = biceps * multiplier;
     }
